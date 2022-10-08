9a5cc19007f8f8a88606677ac193aad73601278c
Dave <dave.cadelina@outlook.com>
2022-10-08 11:46:06 +1100

    added tuples

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
-import fs from 'fs';
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from './MatchResult'
 
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => {
-    return row.split(',')
-})
+const reader = new CsvFileReader("football.csv");
+reader.read();
 
-enum MatchResult {
-    HomeWin = 'H',
-    AwayWin = 'A',
-    Draw = 'D'
-}
+let manUnitedWins = 0;
 
-
-let manUnitedWins = 0
-
-for(let match of matches){
-
-    if(match[1] === "Man United" && match[5] === MatchResult.HomeWin){
-        manUnitedWins++
-    } else if ((match[2] === "Man United" && match[5] === MatchResult.AwayWin)) {
-        manUnitedWins++
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
     }
 }
 
-console.log(`Manchester United won ${manUnitedWins} games`)
+console.log(`Manchester United won ${manUnitedWins} games`);
